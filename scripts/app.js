3e5a11e1059a4153e046686169e979f1f95ebd11
Raul Gutierrez <rcgutierreziii@gmail.com>
2017-09-27 14:50:11 -0500

    map added

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
 // define globals
-var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+let weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+
+let coord = {};
 
 $(document).ready(function() {
 
@@ -15,11 +17,51 @@ $(document).ready(function() {
 
   function onSuccess(earthquakeData){
     earthquakeData.features.forEach(function(element){
-      $('#info').append("<p>" + (element.properties.mag + ' - ' + element.properties.place)+ "</p>");
-      console.log(element.properties.mag + ' - ' + element.properties.place);
+      $('#info').append("<p>" + (element.properties.mag + ' M - ' + element.properties.place)+ "</p>");
+
+      var thisCoord = [];
+
+      for (var i = 2; i > 0; i--) {
+        thisCoord.push(element.geometry.coordinates[i-1]);
+      };
+
     });
 
-    console.log("here");
+
   };
-  getQuakes();
+
+
+
+
+
+
+    function initMap() {
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 17.342349, lng: -30.165751},
+        zoom: 2
+      });
+      var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+      });
+      $('#map').append(map);
+    }
+
+
+
+    initMap();
+
+
+
+
+
+
+
+
+    getQuakes();
+
+
+
+
+
 });
