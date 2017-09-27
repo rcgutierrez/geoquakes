5cbc241433685b3a60998d2ead624ac4f6502970
Raul Gutierrez <rcgutierreziii@gmail.com>
2017-09-27 13:43:59 -0500

    city list added

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,24 @@
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
 $(document).ready(function() {
-  console.log("Let's get coding!");
-  // CODE IN HERE!
 
+  function getQuakes(){
+    $.ajax({
+      method: 'GET',
+      url: weekly_quakes_endpoint,
+      dataType: 'json',
+      success: onSuccess,
+      // error: onError
+    });
+  }
+
+  function onSuccess(earthquakeData){
+    earthquakeData.features.forEach(function(element){
+      $('#info').append("<p>" + (element.properties.mag + ' - ' + element.properties.place)+ "</p>");
+      console.log(element.properties.mag + ' - ' + element.properties.place);
+    });
+
+    console.log("here");
+  };
+  getQuakes();
 });
